3082c6f26660777b41db9cf8b8b7ca646462835a
Amar Kushwaha <kushwahaamar-dev@users.noreply.github.com>
2026-02-20 14:27:20 -0700

    fix: include papi descriptors dist for Vercel deployment

---
+++ .papi/descriptors/dist/descriptors.d.ts
@@ -0,0 +1,1 @@
+export declare const Dot: Record<string, object>;
---
+++ .papi/descriptors/dist/index.mjs
@@ -0,0 +1,271 @@
+// .papi/descriptors/src/common.ts
+var table = new Uint8Array(128);
+for (let i = 0; i < 64; i++) table[i < 26 ? i + 65 : i < 52 ? i + 71 : i < 62 ? i - 4 : i * 4 - 205] = i;
+var toBinary = (base64) => {
+  const n = base64.length, bytes = new Uint8Array((n - Number(base64[n - 1] === "=") - Number(base64[n - 2] === "=")) * 3 / 4 | 0);
+  for (let i2 = 0, j = 0; i2 < n; ) {
+    const c0 = table[base64.charCodeAt(i2++)], c1 = table[base64.charCodeAt(i2++)];
+    const c2 = table[base64.charCodeAt(i2++)], c3 = table[base64.charCodeAt(i2++)];
+    bytes[j++] = c0 << 2 | c1 >> 4;
+    bytes[j++] = c1 << 4 | c2 >> 2;
+    bytes[j++] = c2 << 6 | c3;
+  }
+  return bytes;
+};
+
+// .papi/descriptors/src/dot.ts
+var descriptorValues = import("./descriptors-URMGYS57.mjs").then((module) => module["Dot"]);
+var metadataTypes = import("./metadataTypes-U377QCVQ.mjs").then(
+  (module) => toBinary("default" in module ? module.default : module)
+);
+var asset = {};
+var extensions = {};
+var getMetadata = () => import("./dot_metadata-KMDJ3OKU.mjs").then(
+  (module) => toBinary("default" in module ? module.default : module)
+);
+var genesis = "0x77afd6190f1554ad45fd0d31aee62aacc33c6db0ea801129acb813f913e0764f";
+var _allDescriptors = { descriptors: descriptorValues, metadataTypes, asset, extensions, getMetadata, genesis };
+var dot_default = _allDescriptors;
+
+// .papi/descriptors/src/common-types.ts
+import { _Enum } from "polkadot-api";
+var DigestItem = _Enum;
+var Phase = _Enum;
+var DispatchClass = _Enum;
+var TokenError = _Enum;
+var ArithmeticError = _Enum;
+var TransactionalError = _Enum;
+var PreimageEvent = _Enum;
+var BalanceStatus = _Enum;
+var TransactionPaymentEvent = _Enum;
+var StakingRewardDestination = _Enum;
+var StakingForcing = _Enum;
+var OffencesEvent = _Enum;
+var GrandpaEvent = _Enum;
+var XcmV3Junctions = _Enum;
+var XcmV3Junction = _Enum;
+var XcmV3JunctionNetworkId = _Enum;
+var XcmV3JunctionBodyId = _Enum;
+var XcmV2JunctionBodyPart = _Enum;
+var XcmV3MultiassetAssetId = _Enum;
+var XcmV5Junctions = _Enum;
+var XcmV5Junction = _Enum;
+var XcmV5NetworkId = _Enum;
+var XcmVersionedLocation = _Enum;
+var ConvictionVotingVoteAccountVote = _Enum;
+var PreimagesBounded = _Enum;
+var CommonClaimsEvent = _Enum;
+var ChildBountiesEvent = _Enum;
+var ElectionProviderMultiPhaseEvent = _Enum;
+var ElectionProviderMultiPhaseElectionCompute = _Enum;
+var ElectionProviderMultiPhasePhase = _Enum;
+var BagsListEvent = _Enum;
+var NominationPoolsPoolState = _Enum;
+var NominationPoolsCommissionClaimPermission = _Enum;
+var NominationPoolsClaimPermission = _Enum;
+var ParachainsHrmpEvent = _Enum;
+var ParachainsDisputesEvent = _Enum;
+var ParachainsDisputeLocation = _Enum;
+var ParachainsDisputeResult = _Enum;
+var CommonParasRegistrarEvent = _Enum;
+var CommonSlotsEvent = _Enum;
+var CommonAuctionsEvent = _Enum;
+var PolkadotRuntimeParachainsCoretimeEvent = _Enum;
+var XcmV5Instruction = _Enum;
+var XcmV3MultiassetFungibility = _Enum;
+var XcmV3MultiassetAssetInstance = _Enum;
+var XcmV3MaybeErrorCode = _Enum;
+var XcmV2OriginKind = _Enum;
+var XcmV5AssetFilter = _Enum;
+var XcmV5WildAsset = _Enum;
+var XcmV2MultiassetWildFungibility = _Enum;
+var XcmV3WeightLimit = _Enum;
+var XcmVersionedAssets = _Enum;
+var ParachainsInclusionAggregateMessageOrigin = _Enum;
+var ParachainsInclusionUmpQueueId = _Enum;
+var ParachainsOrigin = _Enum;
+var PreimageOldRequestStatus = _Enum;
+var PreimageRequestStatus = _Enum;
+var BabeDigestsNextConfigDescriptor = _Enum;
+var BabeAllowedSlots = _Enum;
+var BabeDigestsPreDigest = _Enum;
+var BalancesTypesReasons = _Enum;
+var PreimagePalletHoldReason = _Enum;
+var WestendRuntimeRuntimeFreezeReason = _Enum;
+var NominationPoolsPalletFreezeReason = _Enum;
+var TransactionPaymentReleases = _Enum;
+var GrandpaStoredState = _Enum;
+var TreasuryPaymentState = _Enum;
+var ConvictionVotingVoteVoting = _Enum;
+var VotingConviction = _Enum;
+var TraitsScheduleDispatchTime = _Enum;
+var ClaimsStatementKind = _Enum;
+var Version = _Enum;
+var ChildBountyStatus = _Enum;
+var PolkadotPrimitivesV6ExecutorParamsExecutorParam = _Enum;
+var PolkadotPrimitivesV6PvfPrepKind = _Enum;
+var PvfExecKind = _Enum;
+var ValidityAttestation = _Enum;
+var PolkadotPrimitivesV6DisputeStatement = _Enum;
+var PolkadotPrimitivesV6ValidDisputeStatementKind = _Enum;
+var InvalidDisputeStatementKind = _Enum;
+var PolkadotRuntimeParachainsSchedulerCommonAssignment = _Enum;
+var ParachainsParasParaLifecycle = _Enum;
+var UpgradeGoAhead = _Enum;
+var UpgradeRestriction = _Enum;
+var BrokerCoretimeInterfaceCoreAssignment = _Enum;
+var MultiSigner = _Enum;
+var CommonCrowdloanLastContribution = _Enum;
+var XcmV3Response = _Enum;
+var XcmV3TraitsError = _Enum;
+var XcmV4Response = _Enum;
+var XcmPalletVersionMigrationStage = _Enum;
+var XcmVersionedAssetId = _Enum;
+var MultiAddress = _Enum;
+var BalancesAdjustmentDirection = _Enum;
+var StakingPalletConfigOpBig = _Enum;
+var StakingPalletConfigOp = _Enum;
+var GrandpaEquivocation = _Enum;
+var NominationPoolsBondExtra = _Enum;
+var NominationPoolsConfigOp = _Enum;
+var MultiSignature = _Enum;
+var XcmVersionedXcm = _Enum;
+var XcmV3Instruction = _Enum;
+var XcmV3MultiassetMultiAssetFilter = _Enum;
+var XcmV3MultiassetWildMultiAsset = _Enum;
+var XcmV4Instruction = _Enum;
+var XcmV4AssetAssetFilter = _Enum;
+var XcmV4AssetWildAsset = _Enum;
+var ReferendaTypesCurve = _Enum;
+var TransactionValidityUnknownTransaction = _Enum;
+var TransactionValidityTransactionSource = _Enum;
+var OccupiedCoreAssumption = _Enum;
+var SlashingOffenceKind = _Enum;
+var MmrPrimitivesError = _Enum;
+
+// .papi/descriptors/src/index.ts
+var metadatas = { ["0xaae6f831077005fffec66b31a9178628a014d75ea4b448e2290470b6ab81f7d8"]: dot_default };
+var getMetadata2 = async (codeHash) => {
+  try {
+    return await metadatas[codeHash].getMetadata();
+  } catch {
+  }
+  return null;
+};
+export {
+  ArithmeticError,
+  BabeAllowedSlots,
+  BabeDigestsNextConfigDescriptor,
+  BabeDigestsPreDigest,
+  BagsListEvent,
+  BalanceStatus,
+  BalancesAdjustmentDirection,
+  BalancesTypesReasons,
+  BrokerCoretimeInterfaceCoreAssignment,
+  ChildBountiesEvent,
+  ChildBountyStatus,
+  ClaimsStatementKind,
+  CommonAuctionsEvent,
+  CommonClaimsEvent,
+  CommonCrowdloanLastContribution,
+  CommonParasRegistrarEvent,
+  CommonSlotsEvent,
+  ConvictionVotingVoteAccountVote,
+  ConvictionVotingVoteVoting,
+  DigestItem,
+  DispatchClass,
+  ElectionProviderMultiPhaseElectionCompute,
+  ElectionProviderMultiPhaseEvent,
+  ElectionProviderMultiPhasePhase,
+  GrandpaEquivocation,
+  GrandpaEvent,
+  GrandpaStoredState,
+  InvalidDisputeStatementKind,
+  MmrPrimitivesError,
+  MultiAddress,
+  MultiSignature,
+  MultiSigner,
+  NominationPoolsBondExtra,
+  NominationPoolsClaimPermission,
+  NominationPoolsCommissionClaimPermission,
+  NominationPoolsConfigOp,
+  NominationPoolsPalletFreezeReason,
+  NominationPoolsPoolState,
+  OccupiedCoreAssumption,
+  OffencesEvent,
+  ParachainsDisputeLocation,
+  ParachainsDisputeResult,
+  ParachainsDisputesEvent,
+  ParachainsHrmpEvent,
+  ParachainsInclusionAggregateMessageOrigin,
+  ParachainsInclusionUmpQueueId,
+  ParachainsOrigin,
+  ParachainsParasParaLifecycle,
+  Phase,
+  PolkadotPrimitivesV6DisputeStatement,
+  PolkadotPrimitivesV6ExecutorParamsExecutorParam,
+  PolkadotPrimitivesV6PvfPrepKind,
+  PolkadotPrimitivesV6ValidDisputeStatementKind,
+  PolkadotRuntimeParachainsCoretimeEvent,
+  PolkadotRuntimeParachainsSchedulerCommonAssignment,
+  PreimageEvent,
+  PreimageOldRequestStatus,
+  PreimagePalletHoldReason,
+  PreimageRequestStatus,
+  PreimagesBounded,
+  PvfExecKind,
+  ReferendaTypesCurve,
+  SlashingOffenceKind,
+  StakingForcing,
+  StakingPalletConfigOp,
+  StakingPalletConfigOpBig,
+  StakingRewardDestination,
+  TokenError,
+  TraitsScheduleDispatchTime,
+  TransactionPaymentEvent,
+  TransactionPaymentReleases,
+  TransactionValidityTransactionSource,
+  TransactionValidityUnknownTransaction,
+  TransactionalError,
+  TreasuryPaymentState,
+  UpgradeGoAhead,
+  UpgradeRestriction,
+  ValidityAttestation,
+  Version,
+  VotingConviction,
+  WestendRuntimeRuntimeFreezeReason,
+  XcmPalletVersionMigrationStage,
+  XcmV2JunctionBodyPart,
+  XcmV2MultiassetWildFungibility,
+  XcmV2OriginKind,
+  XcmV3Instruction,
+  XcmV3Junction,
+  XcmV3JunctionBodyId,
+  XcmV3JunctionNetworkId,
+  XcmV3Junctions,
+  XcmV3MaybeErrorCode,
+  XcmV3MultiassetAssetId,
+  XcmV3MultiassetAssetInstance,
+  XcmV3MultiassetFungibility,
+  XcmV3MultiassetMultiAssetFilter,
+  XcmV3MultiassetWildMultiAsset,
+  XcmV3Response,
+  XcmV3TraitsError,
+  XcmV3WeightLimit,
+  XcmV4AssetAssetFilter,
+  XcmV4AssetWildAsset,
+  XcmV4Instruction,
+  XcmV4Response,
+  XcmV5AssetFilter,
+  XcmV5Instruction,
+  XcmV5Junction,
+  XcmV5Junctions,
+  XcmV5NetworkId,
+  XcmV5WildAsset,
+  XcmVersionedAssetId,
+  XcmVersionedAssets,
+  XcmVersionedLocation,
+  XcmVersionedXcm,
+  dot_default as dot,
+  getMetadata2 as getMetadata
+};
